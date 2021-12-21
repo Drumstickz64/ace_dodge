a3102134ee853eaeb95ffe6c8767662bade7464f
lunkk <gearman533@gmail.com>
2021-12-21 23:40:12 +0300

    refactor change base_direction to front_direction in SteerMove

--- src/steerer.rs
+++ src/steerer.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 pub struct SteerMove {
     velocity: Vec3,
     /// the direction the front of the Entity is facing when it has no rotation
-    base_direction: Vec3,
+    front_direction: Vec3,
     speed: f32,
 }
 
@@ -11,7 +11,7 @@ impl SteerMove {
     pub fn new(speed: f32, base_direction: Vec3) -> SteerMove {
         SteerMove {
             velocity: base_direction * speed,
-            base_direction,
+            front_direction: base_direction,
             speed,
         }
     }
@@ -19,7 +19,7 @@ impl SteerMove {
 
 fn steerer_movement(mut steerers: Query<(&mut Transform, &mut SteerMove)>) {
     for (mut transform, mut steer_move) in steerers.iter_mut() {
-        let direction = transform.rotation.mul_vec3(steer_move.base_direction);
+        let direction = transform.rotation.mul_vec3(steer_move.front_direction);
         steer_move.velocity = direction * steer_move.speed;
         transform.translation += steer_move.velocity;
     }
